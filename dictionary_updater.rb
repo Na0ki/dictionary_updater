a7bd0437e45b8fd68dc09c0ba656c3398ad89a67
ahiru <wellingtonnmaeda0524@gmail.com>
2017-01-27 18:22:26 +0900

    辞書の自動更新までを実装

--- dictionary_updater.rb
+++ dictionary_updater.rb
@@ -14,7 +14,7 @@ Plugin.create(:dictionary_updater) do
       raise('dictionary directory not found!') unless Dir.exists?(@dic_dir)
 
       # @see https://docs.ruby-lang.org/ja/latest/method/Dir/s/glob.html
-      @dictionaries = Dir.glob("#{@dic_dir}/*.yml")
+      @dictionaries = Dir.glob("#{@dic_dir}/*.yml").map { |d| File.basename(d, '.*') }
     rescue => e
       error e
       Service.primary.post(message: "@#{Service.primary.user} D エラー発生: #{e}")
@@ -23,19 +23,26 @@ Plugin.create(:dictionary_updater) do
 
 
   def update_dictionary(message)
+    prepare
     Thread.new(message) { |m|
-      matched = /@#{m.user.idname}\s辞書追加\s(?<type>.+)\s(?<word>.+)/.match(m)
+      matched = /@#{m.user.idname}\s辞書追加\s(?<type>[\w\-]+)\s(?<word>.+)/.match(m.to_s)
       if matched.nil?
         m.post(message: 'D @ahiru3net 形式: @ahiru3net 辞書追加 辞書の種類 追加文')
       else
         Delayer::Deferred.fail("辞書 #{matched[:type]} は存在しません") unless @dictionaries.include?(matched[:type])
 
         # ファイルに書き出し
-        File.open(File.join(@dic_dir, "#{matched[:type]}.yml")) do |f|
-          f.puts("- \"#{matched[:word]}\"")
+        File.open(File.join(@dic_dir, "#{matched[:type]}.yml"), 'a') do |file|
+          file.puts("- \"#{matched[:word]}\"")
         end
 
-        # TODO: git コマンドでコミット&プッシュを実行するようにする
+        %x( cd #{@dic_dir} && git add "#{matched[:type]}.yml" && git commit -m "辞書更新" && git push origin master )
+        result = $?.success?
+        notice "辞書の更新に#{result ? '成功' : '失敗'}しました"
+        if result
+          m.post(:message => "辞書の追加が完了しました\n辞書更新\n %{time}" % {time: Time.now.to_s, result: result},
+                 :replyto => m)
+        end
       end
     }
   end
@@ -43,6 +50,9 @@ Plugin.create(:dictionary_updater) do
 
   on_appear do |ms|
     ms.each do |m|
+      # メッセージ生成時刻が起動前またはリツイートならば次のループへ
+      next if m[:created] < @defined_time or m.retweet?
+
       # 自分のみに反応する
       next if m.user[:id] != Service.primary.user_obj.id
 
